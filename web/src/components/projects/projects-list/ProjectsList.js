4c5bbee381a353bb92ee473d0825af74fb5f36e3
Carlos del Prado <carlos.delpradomota@telefonica.com>
2023-04-17 19:59:49 +0200

    feat: group project by module

--- web/src/components/projects/projects-list/ProjectsList.js
+++ web/src/components/projects/projects-list/ProjectsList.js
@@ -17,16 +17,37 @@ function ProjectsList() {
       .catch(error => console.error(error))
   }, [searchParams]);
 
+  const projectsByModule = projects.reduce((projectsByModule, project) => {
+    if (!projectsByModule[`${project.module}`]) {
+      projectsByModule[`${project.module}`] = [];
+    } 
+    projectsByModule[`${project.module}`].push(project);
+    return projectsByModule;
+  }, {})
+
   return (
-    <>
-      <div className="row g-2">
-          {projects.map((project) => (
-            <div className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch" key={project.id}>
-              <ProjectItem  project={project} />
+    <div className="accordion accordion-flush" id="module-projects-accordion">
+      {Object.keys(projectsByModule).sort((m1, m2) => m1 - m2).map((module) => (
+        <div key={module} className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-module-${module}`} aria-expanded="true" aria-controls={`flush-module-${module}`}>
+              Module {module}
+            </button>
+          </h2>
+          <div id={`flush-module-${module}`} className="accordion-collapse collapse show">
+            <div className="accordion-body">
+              <div className="row g-2">
+                {projectsByModule[module].map((project) => (
+                  <div className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch" key={project.id}>
+                    <ProjectItem project={project} />
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
-      </div>
-    </>
+          </div>
+        </div>          
+      ))}
+    </div>
   )
 }
 
